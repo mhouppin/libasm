7dedd8bdb6bd9762d33f0723f47a40bccd131120
Morgan Houppin <morganhouppin@gmail.com>
2021-09-13 14:53:24 +0200

    Added ft_read to the library

---
+++ sources/ft_read.s
@@ -0,0 +1,43 @@
+%include "kernel_symbols.s"
+
+section .text
+    global C_FUNCTION(ft_read)
+%ifdef MACOS_KERN
+    extern ___error
+%else
+    extern __errno_location
+%endif
+
+C_FUNCTION(ft_read):
+%ifdef MACOS_KERN
+
+    mov rax, 0x2000003
+    syscall
+    jb .error
+    ret
+
+.error:
+    push rax
+    call ___error
+    pop rdi
+    mov dword [rax], edi
+    mov rax, -1
+    ret
+
+%else
+    mov rax, 0
+    syscall
+    cmp rax, 0
+    jl .error
+    ret
+
+.error:
+    neg rax
+    push rax
+    call __errno_location
+    pop rdi
+    mov dword [rax], edi
+    mov rax, -1
+    ret
+
+%endif
